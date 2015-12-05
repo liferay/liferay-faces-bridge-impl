0b03e62ef978e23f5aedcd9dadcba7f05da4e166
Neil Griffin <neil.griffin.scm@gmail.com>
2015-12-04 19:37:21 -0500

    FACES-2523 Create the jsf-showcase-portlet project by layering it on top of jsf-showcase-webapp

---
+++ demos/bridge/jsf-showcase-portlet/src/main/java/com/liferay/faces/bridge/demos/bean/BridgeInputFileModelBean.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.demos.bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.liferay.faces.bridge.model.UploadedFile;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ViewScoped
+@ManagedBean
+public class BridgeInputFileModelBean implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 211172404377673109L;
+
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(BridgeInputFileModelBean.class);
+
+	// Private Data Members
+	private List<UploadedFile> uploadedFiles;
+
+	@PostConstruct
+	public void postConstruct() {
+		this.uploadedFiles = new ArrayList<UploadedFile>();
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+
+		for (UploadedFile uploadedFile : uploadedFiles) {
+
+			try {
+				uploadedFile.delete();
+			}
+			catch (IOException e) {
+				logger.error(e);
+			}
+		}
+
+		uploadedFiles = null;
+	}
+
+	public List<UploadedFile> getUploadedFiles() {
+		return uploadedFiles;
+	}
+}
