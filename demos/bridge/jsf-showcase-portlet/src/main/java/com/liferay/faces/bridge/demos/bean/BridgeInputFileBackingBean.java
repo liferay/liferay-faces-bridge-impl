ae1f6d129853140378c80e37f4f5e231d431d38f
Neil Griffin <neil.griffin.scm@gmail.com>
2015-12-04 19:43:10 -0500

    FACES-2523 Create the jsf-showcase-portlet project by layering it on top of jsf-showcase-webapp

---
+++ demos/bridge/jsf-showcase-portlet/src/main/java/com/liferay/faces/bridge/demos/bean/BridgeInputFileBackingBean.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.demos.bean;
+
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UICommand;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import com.liferay.faces.bridge.event.FileUploadEvent;
+import com.liferay.faces.bridge.model.UploadedFile;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@RequestScoped
+@ManagedBean
+public class BridgeInputFileBackingBean {
+
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(BridgeInputFileBackingBean.class);
+
+	@ManagedProperty(value = "#{bridgeInputFileModelBean}")
+	private com.liferay.faces.bridge.demos.bean.BridgeInputFileModelBean bridgeInputFileModelBean;
+
+	public void deleteUploadedFile(ActionEvent actionEvent) {
+
+		UICommand uiCommand = (UICommand) actionEvent.getComponent();
+		String fileId = (String) uiCommand.getValue();
+
+		try {
+			List<UploadedFile> uploadedFiles = bridgeInputFileModelBean.getUploadedFiles();
+
+			UploadedFile uploadedFileToDelete = null;
+
+			for (UploadedFile uploadedFile : uploadedFiles) {
+
+				if (uploadedFile.getId().equals(fileId)) {
+					uploadedFileToDelete = uploadedFile;
+
+					break;
+				}
+			}
+
+			if (uploadedFileToDelete != null) {
+				uploadedFileToDelete.delete();
+				uploadedFiles.remove(uploadedFileToDelete);
+				logger.debug("Deleted file=[{0}]", uploadedFileToDelete.getName());
+			}
+		}
+		catch (Exception e) {
+			logger.error(e);
+		}
+	}
+
+	public void handleFileUpload(FileUploadEvent fileUploadEvent) {
+
+		List<UploadedFile> uploadedFiles = bridgeInputFileModelBean.getUploadedFiles();
+		UploadedFile uploadedFile = fileUploadEvent.getUploadedFile();
+
+		if (uploadedFile.getStatus() == UploadedFile.Status.FILE_SAVED) {
+
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			FacesMessage facesMessage = new FacesMessage("Received fileUploadEvent for file named '" +
+					uploadedFile.getName() + "' in the " + fileUploadEvent.getPhaseId().toString() + " phase.");
+			facesContext.addMessage(null, facesMessage);
+			uploadedFiles.add(uploadedFile);
+			logger.debug("Received fileName=[{0}] absolutePath=[{1}]", uploadedFile.getName(),
+				uploadedFile.getAbsolutePath());
+		}
+		else {
+			logger.error("Failed to receive uploaded file due to error status=[{0}] message=[{1}]",
+				uploadedFile.getStatus(), uploadedFile.getMessage());
+		}
+	}
+
+	public void setBridgeInputFileModelBean(
+		com.liferay.faces.bridge.demos.bean.BridgeInputFileModelBean bridgeInputFileModelBean) {
+
+		// Injected via @ManagedProperty annotation
+		this.bridgeInputFileModelBean = bridgeInputFileModelBean;
+	}
+}
---
+++ demos/bridge/jsf-showcase-portlet/src/main/java/com/liferay/faces/bridge/demos/portlet/ActionURLDemoPortlet.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.demos.portlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.faces.render.ResponseStateManager;
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletException;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.faces.GenericFacesPortlet;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ActionURLDemoPortlet extends GenericFacesPortlet {
+
+	@Override
+	public void processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws PortletException,
+		IOException {
+
+		String contentType = actionRequest.getContentType();
+		boolean fileUpload = ((contentType != null) && contentType.toLowerCase().startsWith("multipart/"));
+
+		String viewState = actionRequest.getParameter(ResponseStateManager.VIEW_STATE_PARAM);
+
+		if ((viewState == null) && (!fileUpload)) {
+			actionResponse.setRenderParameter("Non-Faces-Postback", Boolean.TRUE.toString());
+
+			String foo = actionRequest.getParameter("foo");
+
+			if (foo != null) {
+				actionResponse.setRenderParameter("foo", foo);
+			}
+		}
+		else {
+			super.processAction(actionRequest, actionResponse);
+		}
+	}
+
+	@Override
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException,
+		IOException {
+
+		String nonFacesPostback = renderRequest.getParameter("Non-Faces-Postback");
+
+		if (nonFacesPostback == null) {
+			super.doView(renderRequest, renderResponse);
+		}
+		else {
+			PrintWriter writer = renderResponse.getWriter();
+			writer.write("<p>");
+			writer.write("Successfully executed ");
+			writer.write("<strong>non-JSF postback</strong> in ");
+			writer.write(ActionURLDemoPortlet.class.getName());
+			writer.write("</p>");
+
+			String foo = renderRequest.getParameter("foo");
+			writer.write("<p>");
+			writer.write("Action Parameter <strong>foo</strong>=" + foo);
+			writer.write("</p>");
+
+			PortletURL renderURL = renderResponse.createRenderURL();
+			renderURL.setParameter("componentPrefix", "portlet");
+			renderURL.setParameter("componentName", "actionurl");
+			renderURL.setParameter("componentUseCase", "general");
+			writer.write("<p>");
+			writer.write("<a href=\"" + renderURL.toString() + "\">");
+			writer.write("Return to portlet:actionURL in the Liferay Faces Showcase");
+			writer.write("</a>");
+			writer.write("</p>");
+		}
+	}
+}
