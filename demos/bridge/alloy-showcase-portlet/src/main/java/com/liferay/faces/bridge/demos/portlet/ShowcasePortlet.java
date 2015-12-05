9ba9bb1ec90f959b06fd84224fcb93199773b976
Neil Griffin <neil.griffin.scm@gmail.com>
2015-12-04 19:53:24 -0500

    FACES-2524 Create the alloy-showcase-portlet project by layering it on top of alloy-showcase-webapp

---
+++ demos/bridge/alloy-showcase-portlet/src/main/java/com/liferay/faces/bridge/demos/portlet/ShowcasePortlet.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-2015 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.demos.portlet;
+
+import java.io.IOException;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.faces.Bridge;
+import javax.portlet.faces.GenericFacesPortlet;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ShowcasePortlet extends GenericFacesPortlet {
+
+	@Override
+	protected void doView(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException,
+		IOException {
+
+		String componentPrefix = renderRequest.getParameter("componentPrefix");
+		String componentName = renderRequest.getParameter("componentName");
+
+		if ((componentPrefix != null) && (componentName != null)) {
+			String viewId = "/views/component.xhtml";
+			renderRequest.setAttribute(Bridge.VIEW_ID, viewId);
+		}
+
+		super.doView(renderRequest, renderResponse);
+	}
+}
