7e80e2b33c102f5dd4fd56e9bd84e2fd24765d49
Neil Griffin <neil.griffin.scm@gmail.com>
2016-01-21 12:49:14 -0500

    Liferay 7 XPath fixes: signin and removes portlet topper requirements

--- test/integration/issues/bridge/FACES-1638-portlet/src/test/java/com/liferay/faces/test/FACES1638PortletTest.java
+++ test/integration/issues/bridge/FACES-1638-portlet/src/test/java/com/liferay/faces/test/FACES1638PortletTest.java
@@ -90,7 +90,7 @@ public class FACES1638PortletTest extends TesterBase {
 		browser.navigate().to(url);
 		logger.log(Level.INFO, "browser.getTitle() = " + browser.getTitle());
 		logger.log(Level.INFO, "browser.getCurrentUrl() = " + browser.getCurrentUrl());
-		getPortletDisplayName();
+		// getPortletDisplayName();
 		logger.log(Level.INFO, "displayName.getText() = " + displayName.getText());
 		logger.log(Level.INFO, "formTag.getText() = " + formTag.getText());
 
