f9614835a7910a0a0a387ecaacc3345d1a7894a1
Neil Griffin <neil.griffin.scm@gmail.com>
2016-02-10 14:46:00 -0500

    FACES-2594 added a liferay71 profile (code review)

--- tck/bridge-tck-main-portlet/test-liferay71-tomcat.sh
+++ tck/bridge-tck-main-portlet/test-liferay71-tomcat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn surefire-report:report -P tck,liferay,liferay71,tomcat
+mvn surefire-report:report -P tck,liferay,tomcat
--- tck/bridge-tck-main-portlet/test-liferay71-websphere.sh
+++ tck/bridge-tck-main-portlet/test-liferay71-websphere.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn surefire-report:report -P tck,liferay,liferay71,websphere
+mvn surefire-report:report -P tck,liferay,websphere
