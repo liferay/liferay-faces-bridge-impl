7a5a5e0966142fb797530a367853261cd8aac61c
Neil Griffin <neil.griffin.scm@gmail.com>
2016-04-19 18:47:21 -0400

    FACES-2637 Move BridgeURL to com.liferay.faces.bridge and simplify the API by moving implementation details to the bridge implementation (fix NullPointerException in BridgeURLFactoryImpl)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/internal/BridgeURLFactoryImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/internal/BridgeURLFactoryImpl.java
@@ -120,7 +120,7 @@ public class BridgeURLFactoryImpl extends BridgeURLFactory {
 		// Private Data Members
 		public BridgeConfig bridgeConfig;
 		public String contextPath;
-		public String currentFacesViewId = null;
+		public String currentFacesViewId;
 		public String namespace;
 
 		public ContextInfo(FacesContext facesContext) {
