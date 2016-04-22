d533d822c007e11e22c8a01cd4e084c4f137927f
Neil Griffin <neil.griffin.scm@gmail.com>
2016-04-22 18:34:04 -0400

    FACES-2665 Error in Spec requirement for preserving "scoped data" in Section 5.1.2, titled "Managing Lifecycle State"

--- bridge-impl/src/main/java/com/liferay/faces/bridge/internal/BridgeURLBase.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/internal/BridgeURLBase.java
@@ -20,7 +20,6 @@ import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
