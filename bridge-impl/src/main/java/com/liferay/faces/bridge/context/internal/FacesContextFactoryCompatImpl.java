aac3378e70b50bf3dcb568e5861539b56b2e51cb
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-18 13:18:53 -0400

    FACES-2815 Minimize diffs across branches for com.liferay.faces.bridge.context.internal.FacesContextFactoryImpl

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/internal/FacesContextFactoryCompatImpl.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.context.internal;
+
+import javax.faces.FacesException;
+import javax.faces.context.FacesContext;
+import javax.faces.context.FacesContextFactory;
+import javax.faces.lifecycle.Lifecycle;
+import javax.portlet.PortletContext;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public abstract class FacesContextFactoryCompatImpl extends FacesContextFactory {
+
+	@Override
+	public abstract FacesContextFactory getWrapped();
+
+	public FacesContext getFacesContext(PortletContext portletContext, PortletRequest portletRequest,
+		PortletResponse portletResponse, Lifecycle lifecycle) throws FacesException {
+		return getWrapped().getFacesContext(portletContext, portletRequest, portletResponse, lifecycle);
+	}
+}
