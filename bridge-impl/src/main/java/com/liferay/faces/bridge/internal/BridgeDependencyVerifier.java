692606ae35ea31147e0fe6d278ce93e20db8baca
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-18 19:14:36 -0400

    FACES-2548 Log an error if dependencies are not verified at runtime

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/internal/BridgeDependencyVerifier.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.internal;
+
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+import com.liferay.faces.util.product.Product;
+import com.liferay.faces.util.product.ProductFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class BridgeDependencyVerifier {
+
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(BridgeDependencyVerifier.class);
+
+	public static void verify() {
+
+		Package bridgePackage = BridgeDependencyVerifier.class.getPackage();
+		String implementationTitle = bridgePackage.getImplementationTitle();
+		String implementationVersion = bridgePackage.getImplementationVersion();
+		Product liferayPortal = ProductFactory.getProduct(Product.Name.LIFERAY_PORTAL);
+
+		if (liferayPortal.isDetected()) {
+
+			Product liferayFacesBridgeExt = ProductFactory.getProduct(Product.Name.LIFERAY_FACES_BRIDGE_EXT);
+
+			if (!liferayFacesBridgeExt.isDetected()) {
+				logger.error(
+					"{0} {1} is running in Liferay Portal {2}.{3} but the com.liferay.faces.bridge.ext.jar dependency is not in the classpath",
+					implementationTitle, implementationVersion, liferayPortal.getMajorVersion(),
+					liferayPortal.getMinorVersion());
+			}
+		}
+
+		Product jsf = ProductFactory.getProduct(Product.Name.JSF);
+
+		int jsfMajorVersion = jsf.getMajorVersion();
+		int jsfMinorVersion = jsf.getMinorVersion();
+
+		if ((jsfMajorVersion != 2) && (jsfMinorVersion != 1)) {
+			logger.error("{0} {1} is designed to be used with JSF 2.1 but detected {2}.{3}", implementationTitle,
+				implementationVersion, jsfMajorVersion, jsfMinorVersion);
+		}
+	}
+}
