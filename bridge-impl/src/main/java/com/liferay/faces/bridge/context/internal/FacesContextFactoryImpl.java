656941eef020369fb5b54d1d9195376531c5ab52
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-18 13:24:28 -0400

    FACES-2815 Minimize diffs across branches for com.liferay.faces.bridge.context.internal.FacesContextFactoryImpl

--- bridge-impl/src/main/java/com/liferay/faces/bridge/context/internal/FacesContextFactoryImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/internal/FacesContextFactoryImpl.java
@@ -24,22 +24,18 @@ import javax.portlet.PortletContext;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletResponse;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-
-import com.liferay.faces.util.helper.Wrapper;
 
 
 /**
  * @author  Neil Griffin
  */
-public class FacesContextFactoryImpl extends FacesContextFactory implements Wrapper<FacesContextFactory> {
+public class FacesContextFactoryImpl extends FacesContextFactoryCompatImpl {
 
 	// Private Data Members
 	private FacesContextFactory wrappedFacesContextFactory;
 
 	public FacesContextFactoryImpl(FacesContextFactory facesContextFactory) {
-		this.wrappedFacesContextFactory = facesContextFactory;
+		wrappedFacesContextFactory = facesContextFactory;
 	}
 
 	@Override
@@ -49,22 +45,12 @@ public class FacesContextFactoryImpl extends FacesContextFactory implements Wrap
 		// If this is a request coming from the portlet container, then return an instance of FacesContext that is
 		// compatible with the portlet lifecycle.
 		if ((context != null) && (context instanceof PortletContext)) {
-			PortletContext portletContext = (PortletContext) context;
-			PortletRequest portletRequest = (PortletRequest) request;
-			String requestContextPath = portletRequest.getContextPath();
-			PortletResponse portletResponse = (PortletResponse) response;
-			ServletContext servletContext = new ServletContextAdapterImpl(portletContext, requestContextPath);
-			ServletRequest servletRequest = new ServletRequestAdapterImpl(portletRequest);
-			ServletResponse servletResponse = new HttpServletResponseAdapterImpl(portletResponse);
-			FacesContext wrappedFacesContext = wrappedFacesContextFactory.getFacesContext(servletContext,
-					servletRequest, servletResponse, lifecycle);
 
-			ExternalContext externalContext = new ExternalContextImpl(portletContext, portletRequest, portletResponse);
-
-			return new FacesContextImpl(wrappedFacesContext, externalContext);
+			return getFacesContext((PortletContext) context, (PortletRequest) request, (PortletResponse) response,
+				lifecycle);
 		}
 
-		// If the specified context is a ServletContext, then it is possible that the session is expiring.
+		// Otherwise, if the specified context is a ServletContext, then it is possible that the session is expiring.
 		else if ((context != null) && (context instanceof ServletContext)) {
 
 			// If the session is expiring, then return an instance of FacesContext that can function in a limited
@@ -84,7 +70,7 @@ public class FacesContextFactoryImpl extends FacesContextFactory implements Wrap
 			// NOTE: BridgeSessionListener creates classes named HttpServletRequestExpirationImpl and
 			// HttpServletResponseExpirationImpl.
 			if ((requestFQCN.length() == 0) || (responseFQCN.length() == 0) || requestFQCN.contains("expiration") ||
-					responseFQCN.contains("expiration")) {
+				responseFQCN.contains("expiration")) {
 
 				ExternalContext externalContext = new ExternalContextExpirationImpl((ServletContext) context);
 
@@ -93,6 +79,7 @@ public class FacesContextFactoryImpl extends FacesContextFactory implements Wrap
 
 			// Otherwise, delegate to the wrapped factory.
 			else {
+
 				return wrappedFacesContextFactory.getFacesContext(context, request, response, lifecycle);
 			}
 		}
@@ -103,8 +90,8 @@ public class FacesContextFactoryImpl extends FacesContextFactory implements Wrap
 		}
 	}
 
+	@Override
 	public FacesContextFactory getWrapped() {
 		return wrappedFacesContextFactory;
 	}
-
 }
