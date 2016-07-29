343e613c26e175638b053a357110bc77596973cd
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-29 17:49:26 -0400

    FACES-2613 Delete the requirements in chapter 7 of the Spec titled "WriteBehindResponse" and associated API & TCK Tests

--- bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ViewHandlerImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ViewHandlerImpl.java
@@ -15,12 +15,8 @@
  */
 package com.liferay.faces.bridge.application.internal;
 
-import java.io.IOException;
-
-import javax.faces.FacesException;
 import javax.faces.application.ViewHandler;
 import javax.faces.component.UIViewRoot;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import com.liferay.faces.util.logging.Logger;
@@ -127,59 +123,9 @@ public class ViewHandlerImpl extends ViewHandlerCompatImpl {
 	}
 
 	@Override
-	public void renderView(FacesContext facesContext, UIViewRoot uiViewRoot) throws IOException, FacesException {
-
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-
-		// Delegate the render to the view-handler chain-of-responsibility.
-		try {
-			_delegateRenderView(facesContext, externalContext, uiViewRoot);
-		}
-
-		// If an exception is thrown, then execute the Mojarra/MyFaces render directly, bypassing the view-handler
-		// chain-of-responsibility.
-		catch (FacesException e) {
-
-			ViewHandler viewHandler = getFacesRuntimeViewHandler();
-
-			if (JspViewHandlerCompat.isJspView(uiViewRoot)) {
-				viewHandler = new JspViewHandlerCompat(viewHandler);
-			}
-
-			viewHandler.renderView(facesContext, uiViewRoot);
-		}
-	}
-
-	@Override
 	public UIViewRoot restoreView(FacesContext facesContext, String viewId) {
 		logger.debug("Restoring view for viewId=[{0}]", viewId);
 
 		return super.restoreView(facesContext, viewId);
-	}
-
-	protected void _delegateRenderView(FacesContext facesContext, ExternalContext externalContext,
-		UIViewRoot uiViewRoot) throws IOException, FacesException {
-
-		// Section 7.1 of the JSR 301 Spec requires that there be legacy support for the use of a servlet-filter in
-		// order to capture plain HTML markup that may appear after the closing </f:view> component tag (a.k.a. "after
-		// view markup"). This requirement was kept in JSR 329 for backward-compatibility. The servlet-filter approach
-		// was necessary in Portlet 1.0 environments since wrapper classes like PortletResponseWrapper were not
-		// available until Portlet 2.0.
-//		Map<String, Object> requestAttributeMap = externalContext.getRequestMap();
-//		requestAttributeMap.put(Bridge.RENDER_CONTENT_AFTER_VIEW, Boolean.TRUE);
-
-//		PortletResponse portletResponse = (PortletResponse) externalContext.getResponse();
-
-		if (JspViewHandlerCompat.isJspView(uiViewRoot)) {
-			ViewHandler jspViewHanlder = new JspViewHandlerCompat(wrappedViewHandler);
-			jspViewHanlder.renderView(facesContext, uiViewRoot);
-		}
-		else {
-			wrappedViewHandler.renderView(facesContext, uiViewRoot);
-		}
-
-//		requestAttributeMap.remove(Bridge.RENDER_CONTENT_AFTER_VIEW);
-//		externalContext.setResponse(portletResponse);
-
 	}
 }
