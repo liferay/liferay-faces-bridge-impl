22b1f20f93fad2c4a7a7ddf003f8d68aedc949b2
Neil Griffin <neil.griffin.scm@gmail.com>
2016-07-29 18:03:21 -0400

    FACES-2613 Delete the requirements in chapter 7 of the Spec titled "WriteBehindResponse" and associated API & TCK Tests (minimize diffs across branches)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ViewHandlerCompatImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/application/internal/ViewHandlerCompatImpl.java
@@ -15,14 +15,24 @@
  */
 package com.liferay.faces.bridge.application.internal;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Map;
 
 import javax.el.ELContext;
 import javax.el.ExpressionFactory;
 import javax.el.ValueExpression;
+import javax.faces.FacesException;
 import javax.faces.application.ViewHandler;
 import javax.faces.application.ViewHandlerWrapper;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.portlet.PortletResponse;
+import javax.portlet.faces.Bridge;
+
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
 
 
 /**
@@ -32,11 +42,108 @@ import javax.faces.context.FacesContext;
  */
 public abstract class ViewHandlerCompatImpl extends ViewHandlerWrapper {
 
-	// Public Constants
-	public static final String RESPONSE_CHARACTER_ENCODING = "com.liferay.faces.bridge.responseCharacterEncoding";
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(ViewHandlerCompatImpl.class);
 
 	// Private Constants
 	private static final String EL_EXPRESSION_PREFIX = "#{";
+
+	@Override
+	public void renderView(FacesContext facesContext, UIViewRoot uiViewRoot) throws IOException, FacesException {
+
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		String initParam = externalContext.getInitParameter(Bridge.RENDER_POLICY);
+		Bridge.BridgeRenderPolicy bridgeRenderPolicy = Bridge.BridgeRenderPolicy.DEFAULT;
+
+		if (initParam != null) {
+			bridgeRenderPolicy = Bridge.BridgeRenderPolicy.valueOf(initParam);
+		}
+
+		// If the developer has specified ALWAYS_DELEGATE in the WEB-INF/web.xml descriptor, then execute
+		// delegate to the ViewHandler chain-of-responsibility.
+		if (bridgeRenderPolicy == Bridge.BridgeRenderPolicy.ALWAYS_DELEGATE) {
+			_delegateRenderView(facesContext, externalContext, uiViewRoot);
+		}
+
+		// If the specified render policy is NEVER_DELEGATE, then do not delegate rendering to the delegation chain.
+		// In this case, JSR 329 expected that the bridge would execute its own implementation of ViewHandler. However,
+		// this proved to be unnecessary. Instead, executing the ViewHandler provided by the Faces runtime works fine,
+		// provided that the bridge work around some Servlet-API dependencies (such as casting to HttpServletRequest)
+		// in the Faces runtime.
+		else if (bridgeRenderPolicy == Bridge.BridgeRenderPolicy.NEVER_DELEGATE) {
+
+			ViewHandler viewHandler = getFacesRuntimeViewHandler();
+
+			if (JspViewHandlerCompat.isJspView(uiViewRoot)) {
+				viewHandler = new JspViewHandlerCompat(viewHandler);
+			}
+
+			viewHandler.renderView(facesContext, uiViewRoot);
+		}
+
+		// Otherwise,
+		else {
+
+			// Delegate the render to the view-handler chain-of-responsibility.
+			try {
+				_delegateRenderView(facesContext, externalContext, uiViewRoot);
+			}
+
+			// If an exception is thrown, then execute the Mojarra/MyFaces render directly, bypassing the view-handler
+			// chain-of-responsibility.
+			catch (FacesException e) {
+
+				ViewHandler viewHandler = getFacesRuntimeViewHandler();
+
+				if (JspViewHandlerCompat.isJspView(uiViewRoot)) {
+					viewHandler = new JspViewHandlerCompat(viewHandler);
+				}
+
+				viewHandler.renderView(facesContext, uiViewRoot);
+			}
+		}
+	}
+
+	protected void _delegateRenderView(FacesContext facesContext, ExternalContext externalContext,
+		UIViewRoot uiViewRoot) throws IOException, FacesException {
+
+		// Section 7.1 of the JSR 301 Spec requires that there be legacy support for the use of a servlet-filter in
+		// order to capture plain HTML markup that may appear after the closing </f:view> component tag (a.k.a. "after
+		// view markup"). This requirement was kept in JSR 329 for backward-compatibility. The servlet-filter approach
+		// was necessary in Portlet 1.0 environments since wrapper classes like PortletResponseWrapper were not
+		// available until Portlet 2.0.
+		Map<String, Object> requestAttributeMap = externalContext.getRequestMap();
+		requestAttributeMap.put(Bridge.RENDER_CONTENT_AFTER_VIEW, Boolean.TRUE);
+
+		PortletResponse portletResponse = (PortletResponse) externalContext.getResponse();
+
+		if (JspViewHandlerCompat.isJspView(uiViewRoot)) {
+			ViewHandler jspViewHanlder = new JspViewHandlerCompat(getWrapped());
+			jspViewHanlder.renderView(facesContext, uiViewRoot);
+		}
+		else {
+			getWrapped().renderView(facesContext, uiViewRoot);
+		}
+
+		requestAttributeMap.remove(Bridge.RENDER_CONTENT_AFTER_VIEW);
+		externalContext.setResponse(portletResponse);
+
+		// TCK TestPage201: renderContentAfterViewTest
+		Object afterViewContent = requestAttributeMap.get(Bridge.AFTER_VIEW_CONTENT);
+
+		if (afterViewContent != null) {
+
+			if (afterViewContent instanceof char[]) {
+				facesContext.getResponseWriter().write((char[]) afterViewContent);
+			}
+			else if (afterViewContent instanceof byte[]) {
+				facesContext.getResponseWriter().write(new String((byte[]) afterViewContent));
+			}
+			else {
+				logger.error("Invalid type for {0}={1}", Bridge.AFTER_VIEW_CONTENT, afterViewContent.getClass());
+			}
+		}
+	}
 
 	/**
 	 * Mojarra 1.x does not have the ability to process faces-config navigation-rule entries with to-view-id containing
