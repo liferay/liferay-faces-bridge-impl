fed67a60e9f4f2ad9b5ed1552af33060740e0d12
Neil Griffin <neil.griffin.scm@gmail.com>
2016-10-12 19:59:27 -0400

    FACES-2938 Upgrade Bridge integration tests to the new test framework

---
+++ issue/FACES-1618-portlet/src/main/java/com/liferay/faces/bridge/issue/RenderKitFactoryImpl.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.issue;
+
+import java.util.Iterator;
+
+import javax.faces.context.FacesContext;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
+
+import com.liferay.faces.util.application.ApplicationUtil;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class RenderKitFactoryImpl extends RenderKitFactory {
+
+	// Private Data Members
+	private RenderKitFactory wrappedRenderKitFactory;
+
+	public RenderKitFactoryImpl(RenderKitFactory renderKitFactory) {
+		this.wrappedRenderKitFactory = renderKitFactory;
+	}
+
+	@Override
+	public void addRenderKit(String renderKitId, RenderKit renderKit) {
+		wrappedRenderKitFactory.addRenderKit(renderKitId, renderKit);
+	}
+
+	@Override
+	public RenderKit getRenderKit(FacesContext facesContext, String renderKitId) {
+
+		RenderKit renderKit = wrappedRenderKitFactory.getRenderKit(facesContext, renderKitId);
+
+		// FACES-2615 Only Add the RenderKit to the delegation chain when the application is not starting up or
+		// shutting down.
+		if (("HTML_BASIC".equals(renderKitId) || "PRIMEFACES_MOBILE".equals(renderKitId)) &&
+				!ApplicationUtil.isStartupOrShutdown(facesContext)) {
+			return new RenderKitImpl(renderKit);
+		}
+		else {
+			return renderKit;
+		}
+	}
+
+	@Override
+	public Iterator<String> getRenderKitIds() {
+		return wrappedRenderKitFactory.getRenderKitIds();
+	}
+}
---
+++ test/integration/src/test/java/com/liferay/faces/bridge/test/integration/demo/applicant/AlloyApplicantPortletTester.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.test.integration.demo.applicant;
+
+import com.liferay.faces.test.selenium.Browser;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class AlloyApplicantPortletTester extends BridgeApplicantPortletTester {
+
+	@Override
+	protected String getFieldErrorXpath(String fieldXpath) {
+
+		String errorMessageXpath =
+			"/span[contains(@class,'alloy-message')][contains(@class,'help-inline') or contains(@class,'help-block')]";
+
+		return "(" + fieldXpath + errorMessageXpath + "|" + fieldXpath + "/.." + errorMessageXpath + "|" + fieldXpath +
+			"/../.." + errorMessageXpath + ")";
+	}
+
+	@Override
+	protected String getPortletPageName() {
+		return "alloy-applicant";
+	}
+
+	@Override
+	protected String getPreferencesResetButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Reset')]";
+	}
+
+	@Override
+	protected String getPreferencesSubmitButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
+	@Override
+	protected String getSubmitAnotherApplicationButton() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit Another Application')]";
+	}
+
+	@Override
+	protected String getSubmitButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
+	@Override
+	protected String getSubmitFileButtonXpath() {
+		return
+			"//form[@method='post'][@enctype='multipart/form-data']/button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
+	@Override
+	protected String getUploadedFileXpath() {
+		return
+			"//h3[contains(text(),'Attachments')]/following-sibling::table[contains(@class,'alloy-data-table')]/tbody/tr/td[2]";
+	}
+
+	@Override
+	protected void selectDate(Browser browser) {
+
+		String calendarButtonXpath =
+			"//button[contains(@id,'dateOfBirth')]/span[contains(@class,'calendar')][not(text())]";
+		browser.centerElementInView(calendarButtonXpath);
+		browser.click(calendarButtonXpath);
+
+		String dateElementXpath =
+			"//table[contains(@class, 'yui3-calendar-grid')]//td[contains(text(),'14')][not(contains(@class,'yui3-calendar-column-hidden'))]";
+		browser.waitForElementVisible(dateElementXpath);
+		browser.click(dateElementXpath);
+	}
+}
