90ac8327bf1568935572cd0af656eecdf44a8a18
Neil Griffin <neil.griffin.scm@gmail.com>
2016-10-14 14:41:37 -0400

    FACES-2696 Utilize Portlet 3.0 dynamic resource dependency feature (temporarily cause the bridge to render resources to the body for Pluto since integration with the HEADER_PHASE is not yet complete)

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/context/internal/PortalContextPlutoImpl.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.context.internal;
+
+import javax.portlet.PortalContext;
+
+import com.liferay.faces.bridge.context.BridgePortalContext;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class PortalContextPlutoImpl extends PortalContextBridgeImpl {
+
+	public PortalContextPlutoImpl(PortalContext portalContext) {
+		super(portalContext);
+	}
+
+	@Override
+	public String getProperty(String name) {
+
+		if ((name != null) && name.endsWith("head.support")) {
+			return null;
+		}
+		else if (BridgePortalContext.STRICT_NAMESPACED_PARAMETERS_SUPPORT.equals(name)) {
+			return Boolean.TRUE.toString();
+		}
+		else {
+			return super.getProperty(name);
+		}
+	}
+}
