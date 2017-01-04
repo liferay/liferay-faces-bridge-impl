5036cbbd0f4e8aaceb3899328035a7f8b18cc511
Neil Griffin <neil.griffin.scm@gmail.com>
2017-01-04 18:04:05 -0500

    FACES-2975 Remove workaround for JAVASERVERFACES-SPEC-PUBLIC-790

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/renderkit/html_basic/internal/RenderKitBridgeImplCompat.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.renderkit.html_basic.internal;
+
+import java.io.Writer;
+
+import javax.faces.context.ResponseWriter;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitWrapper;
+
+import com.liferay.faces.bridge.renderkit.bridge.internal.ResponseWriterBridgeImpl;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class RenderKitBridgeImplCompat extends RenderKitWrapper {
+
+	// Private Data Members
+	private RenderKit wrappedRenderKit;
+
+	public RenderKitBridgeImplCompat(RenderKit wrappedRenderKit) {
+		this.wrappedRenderKit = wrappedRenderKit;
+	}
+
+	/**
+	 * Provides the bridge with the ability to wrap the HTML_BASIC ResponseWriter provided by the JSF implementation.
+	 */
+	public ResponseWriter createResponseWriter(Writer writer, String contentTypeList, String characterEncoding) {
+		ResponseWriter wrappedResponseWriter = wrappedRenderKit.createResponseWriter(writer, contentTypeList,
+				characterEncoding);
+
+		return new ResponseWriterBridgeImpl(wrappedResponseWriter);
+	}
+
+	@Override
+	public RenderKit getWrapped() {
+		return wrappedRenderKit;
+	}
+}
