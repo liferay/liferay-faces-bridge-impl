217b9fdff08c25f785d447dafc702a3c33ab9b36
Neil Griffin <neil.griffin.scm@gmail.com>
2017-01-12 16:46:40 -0500

    FACES-2988 Author test for the com.liferay.faces.issue.1433.portlet

---
+++ issue/JSFSPEC-1433-portlet/src/main/java/com/liferay/faces/issues/bean/TestBean.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.issues.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ManagedBean
+@ViewScoped
+public class TestBean {
+
+	private String firstName;
+	private String lastName;
+	private String middleName;
+
+	public void action() {
+		FacesMessage facesMessage = new FacesMessage("Hello " + firstName + " " + lastName);
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		facesContext.addMessage(null, facesMessage);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+}
