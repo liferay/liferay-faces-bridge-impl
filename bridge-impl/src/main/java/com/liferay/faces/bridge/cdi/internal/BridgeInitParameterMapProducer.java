4d36689f67491f76ea5b32a3c7df65e1ad43a84a
Neil Griffin <neil.griffin.scm@gmail.com>
2017-02-09 17:53:44 -0500

    FACES-2997 ContextNotActiveException thrown when trying to use JSF 2.3 EL keywords registered via CDI producers (code review)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/cdi/internal/BridgeInitParameterMapProducer.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/cdi/internal/BridgeInitParameterMapProducer.java
@@ -1,4 +1,21 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.liferay.faces.bridge.cdi.internal;
+
+import java.util.Map;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
@@ -9,12 +26,14 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.interceptor.Interceptor;
 import javax.portlet.annotations.PortletRequestScoped;
-import java.util.Map;
 
-// alternative to com.sun.faces.cdi.InitParameterMapProducer
+
+/**
+ * Alternative to com.sun.faces.cdi.InitParameterMapProducer
+ */
 @Alternative
 @Dependent
-@Priority(Interceptor.Priority.APPLICATION+10)
+@Priority(Interceptor.Priority.APPLICATION + 10)
 public class BridgeInitParameterMapProducer {
 
 	@InitParameterMap
@@ -25,4 +44,3 @@ public class BridgeInitParameterMapProducer {
 		return FacesContext.getCurrentInstance().getExternalContext().getInitParameterMap();
 	}
 }
-
--- demo/jsf-cdi-applicant-portlet/src/main/java/com/liferay/faces/demos/bean/TestProducersBean.java
+++ demo/jsf-cdi-applicant-portlet/src/main/java/com/liferay/faces/demos/bean/TestProducersBean.java
@@ -1,9 +1,34 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.liferay.faces.demos.bean;
 
-import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
-import javax.faces.annotation.*;
+
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.HeaderMap;
+import javax.faces.annotation.HeaderValuesMap;
+import javax.faces.annotation.InitParameterMap;
+import javax.faces.annotation.RequestCookieMap;
+import javax.faces.annotation.RequestMap;
+import javax.faces.annotation.RequestParameterMap;
+import javax.faces.annotation.RequestParameterValuesMap;
+import javax.faces.annotation.SessionMap;
+import javax.faces.annotation.ViewMap;
 import javax.faces.application.ResourceHandler;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExternalContext;
@@ -11,79 +36,133 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.Map;
 
-@FacesConfig(version = JSF_2_3)
+
+/**
+ * @author  Neil Griffin
+ */
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
 @Named
 @RequestScoped
 public class TestProducersBean {
 
-    @Inject
-    private ExternalContext externalContext;
+	@Inject
+	private ExternalContext externalContext;
 
-    @Inject
-    private FacesContext facesContext;
+	@Inject
+	private FacesContext facesContext;
 
-    @Inject
-    private Flash flash;
+	@Inject
+	private Flash flash;
 
-    @HeaderMap
-    @Inject
-    private Map<String, String> headerMap;
+	@HeaderMap
+	@Inject
+	private Map<String, String> headerMap;
 
-    @HeaderValuesMap
-    @Inject
-    private Map<String, String[]> headerValuesMap;
+	@HeaderValuesMap
+	@Inject
+	private Map<String, String[]> headerValuesMap;
 
-    @InitParameterMap
-    @Inject
-    private Map<String, String> initParameterMap;
+	@InitParameterMap
+	@Inject
+	private Map<String, String> initParameterMap;
 
-    @Inject
-    @RequestCookieMap
-    private Map<String, Object> requestCookieMap;
+	@Inject
+	@RequestCookieMap
+	private Map<String, Object> requestCookieMap;
 
-    @Inject
-    @RequestMap
-    private Map<String, Object> requestMap;
+	@Inject
+	@RequestMap
+	private Map<String, Object> requestMap;
 
-    @Inject
-    @RequestParameterMap
-    private Map<String, String> requestParameterMap;
+	@Inject
+	@RequestParameterMap
+	private Map<String, String> requestParameterMap;
 
-    @Inject
-    @RequestParameterValuesMap
-    private Map<String, String[]> requestParameterValuesMap;
+	@Inject
+	@RequestParameterValuesMap
+	private Map<String, String[]> requestParameterValuesMap;
 
-    @Inject
-    private ResourceHandler resourceHandler;
+	@Inject
+	private ResourceHandler resourceHandler;
 
-    @Inject
-    @SessionMap
-    private Map<String, Object> sessionMap;
+	@Inject
+	@SessionMap
+	private Map<String, Object> sessionMap;
 
-    @Inject
-    @ViewMap
-    private Map<String, Object> viewMap;
+	@Inject
+	@ViewMap
+	private Map<String, Object> viewMap;
 
-    @Inject
-    private UIViewRoot view;
+	@Inject
+	private UIViewRoot view;
 
-    public String getResults() {
-        String returnValue = "success";
-        if (externalContext == null) { returnValue = "failure ... externalContext == null"; }
-        if (facesContext == null) { returnValue = "failure ... facesContext == null"; }
-        if (flash == null) { returnValue = "failure ... flash == null"; }
-        if (headerMap == null) { returnValue = "failure ... headerMap == null"; }
-        if (headerValuesMap == null) { returnValue = "failure ... headerValuesMap == null"; }
-        if (initParameterMap == null) { returnValue = "failure ... initParameterMap == null"; }
-        if (requestCookieMap == null) { returnValue = "failure ... requestCookieMap == null"; }
-        if (requestMap == null) { returnValue = "failure ... requestMap == null"; }
-        if (requestParameterMap == null) { returnValue = "failure ... requestParameterMap == null"; }
-        if (requestParameterValuesMap == null) { returnValue = "failure ... requestParameterValuesMap == null"; }
-        if (resourceHandler == null) { returnValue = "failure ... resourceHandler == null"; }
-        if (sessionMap == null) { returnValue = "failure ... sessionMap == null"; }
-        if (view == null) { returnValue = "failure ... view == null"; }
-        return returnValue;
-    }
+	// Private Data Members
+	private String results;
+
+	public String getResults() {
+
+		if (results == null) {
+
+			results = "";
+
+			if (externalContext == null) {
+				results += "Failure: externalContext == null";
+			}
+
+			if (facesContext == null) {
+				results += " | Failure: facesContext == null";
+			}
+
+			if (flash == null) {
+				results += " | Failure: flash == null";
+			}
+
+			if (headerMap == null) {
+				results += " | Failure: headerMap == null";
+			}
+
+			if (headerValuesMap == null) {
+				results += " | Failure: headerValuesMap == null";
+			}
+
+			if (initParameterMap == null) {
+				results += " | Failure: initParameterMap == null";
+			}
+
+			if (requestCookieMap == null) {
+				results += " | Failure: requestCookieMap == null";
+			}
+
+			if (requestMap == null) {
+				results += " | Failure: requestMap == null";
+			}
+
+			if (requestParameterMap == null) {
+				results += " | Failure: requestParameterMap == null";
+			}
+
+			if (requestParameterValuesMap == null) {
+				results += " | Failure: requestParameterValuesMap == null";
+			}
+
+			if (resourceHandler == null) {
+				results += " | Failure: resourceHandler == null";
+			}
+
+			if (sessionMap == null) {
+				results += " | Failure: sessionMap == null";
+			}
+
+			if (view == null) {
+				results += " | Failure: view == null";
+			}
+
+			if (results.length() == 0) {
+				results = "Success";
+			}
+		}
+
+		return results;
+	}
 }
