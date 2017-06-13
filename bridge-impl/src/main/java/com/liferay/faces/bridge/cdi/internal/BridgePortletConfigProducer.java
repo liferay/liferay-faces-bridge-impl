44466869d89d3b0ec9247abfaff99843a1a85ab3
Neil Griffin <neil.griffin.scm@gmail.com>
2017-06-13 19:55:46 -0400

    FACES-3107 Require the FacesBridge to provide @Alternative CDI producers for Portlet 3.0 @Named beans that can be wrapped by factories

---
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/cdi/internal/BridgePortletConfigProducer.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.cdi.internal;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Produces;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.interceptor.Interceptor;
+import javax.portlet.PortletConfig;
+import javax.portlet.annotations.PortletRequestScoped;
+
+import com.liferay.faces.bridge.util.internal.RequestMapUtil;
+
+
+/**
+ * Alternative to the producer provided by the portlet container.
+ */
+@Alternative
+@Dependent
+@Priority(Interceptor.Priority.APPLICATION + 10)
+public class BridgePortletConfigProducer {
+
+	@Named(value = "portletConfig")
+	@PortletRequestScoped
+	@Produces
+	public PortletConfig getPortletConfig() {
+		return RequestMapUtil.getPortletConfig(FacesContext.getCurrentInstance());
+	}
+}
