56672fcc7526df9394f43cc3c8191d25f4eedbe6
Neil Griffin <neil.griffin.scm@gmail.com>
2017-06-14 16:57:33 -0400

    FACES-2684 Add BridgePortletConfigFactory to the Bridge API (removing BridgePortletContextFactory.java since it proved to be unnecessary)

--- tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/filter/PortletContextTCKImpl.java
+++ tck/bridge-tck-compat/src/main/java/com/liferay/faces/bridge/tck/filter/PortletContextTCKImpl.java
@@ -15,12 +15,6 @@
  */
 package com.liferay.faces.bridge.tck.filter;
 
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Enumeration;
-import java.util.Set;
-
 import javax.portlet.PortletContext;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.filter.PortletContextWrapper;
@@ -36,122 +30,7 @@ public class PortletContextTCKImpl extends PortletContextWrapper {
 	}
 
 	@Override
-	public Object getAttribute(String name) {
-		return getPortletContext().getAttribute(name);
-	}
-
-	@Override
-	public Enumeration<String> getAttributeNames() {
-		return getPortletContext().getAttributeNames();
-	}
-
-	@Override
-	public ClassLoader getClassLoader() {
-		return getPortletContext().getClassLoader();
-	}
-
-	@Override
-	public Enumeration<String> getContainerRuntimeOptions() {
-		return getPortletContext().getContainerRuntimeOptions();
-	}
-
-	@Override
-	public String getContextPath() {
-		return getPortletContext().getContextPath();
-	}
-
-	@Override
-	public int getEffectiveMajorVersion() {
-		return getPortletContext().getEffectiveMajorVersion();
-	}
-
-	@Override
-	public int getEffectiveMinorVersion() {
-		return getPortletContext().getEffectiveMinorVersion();
-	}
-
-	@Override
-	public String getInitParameter(String name) {
-		return getPortletContext().getInitParameter(name);
-	}
-
-	@Override
-	public Enumeration<String> getInitParameterNames() {
-		return getPortletContext().getInitParameterNames();
-	}
-
-	@Override
-	public int getMajorVersion() {
-		return getPortletContext().getMajorVersion();
-	}
-
-	@Override
-	public String getMimeType(String file) {
-		return getPortletContext().getMimeType(file);
-	}
-
-	@Override
-	public int getMinorVersion() {
-		return getPortletContext().getMinorVersion();
-	}
-
-	@Override
-	public PortletRequestDispatcher getNamedDispatcher(String name) {
-		return getPortletContext().getNamedDispatcher(name);
-	}
-
-	@Override
-	public String getPortletContextName() {
-		return getPortletContext().getPortletContextName();
-	}
-
-	@Override
-	public String getRealPath(String path) {
-		return getPortletContext().getRealPath(path);
-	}
-
-	@Override
 	public PortletRequestDispatcher getRequestDispatcher(String path) {
 		return new PortletRequestDispatcherTCKImpl(getPortletContext().getRequestDispatcher(path));
-	}
-
-	@Override
-	public URL getResource(String path) throws MalformedURLException {
-		return getPortletContext().getResource(path);
-	}
-
-	@Override
-	public InputStream getResourceAsStream(String path) {
-		return getPortletContext().getResourceAsStream(path);
-	}
-
-	@Override
-	public Set<String> getResourcePaths(String path) {
-		return getPortletContext().getResourcePaths(path);
-	}
-
-	@Override
-	public String getServerInfo() {
-		return getPortletContext().getServerInfo();
-	}
-
-	@Override
-	public void log(String msg) {
-		getPortletContext().log(msg);
-	}
-
-	@Override
-	public void log(String message, Throwable throwable) {
-		getPortletContext().log(message, throwable);
-	}
-
-	@Override
-	public void removeAttribute(String name) {
-		getPortletContext().removeAttribute(name);
-	}
-
-	@Override
-	public void setAttribute(String name, Object object) {
-		getPortletContext().setAttribute(name, object);
 	}
 }
