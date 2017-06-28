51bc56263ea91701811951e8e19a1d1ab7c2f105
Neil Griffin <neil.griffin.scm@gmail.com>
2017-06-28 18:08:08 -0400

    FACES-3120 Update the bridge version to "Portlet 3.0 Bridge for JavaServer Faces 2.2"

--- tck/bridge-tck-main-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter_3/BridgeVersionTestPortlet.java
+++ tck/bridge-tck-main-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter_3/BridgeVersionTestPortlet.java
@@ -51,14 +51,14 @@ public class BridgeVersionTestPortlet extends GenericPortlet {
 			String name = c.getPackage().getSpecificationTitle();
 			String version = c.getPackage().getSpecificationVersion();
 
-			if ((name == null) || !name.equals("Portlet 2.0 Bridge for JavaServer Faces 1.2")) {
+			if ((name == null) || !name.equals("Portlet 3.0 Bridge for JavaServer Faces 2.2")) {
 				resultWriter.setStatus(BridgeTCKResultWriter.FAIL);
 				resultWriter.setDetail("Incorrect Specification Title: " + name +
-					" should be 'Portlet 2.0 Bridge for JavaServer Faces 1.2'");
+					" should be 'Portlet 3.0 Bridge for JavaServer Faces 2.2'");
 			}
-			else if ((version == null) || !version.equals("1.0")) {
+			else if ((version == null) || !version.equals("5.0.0")) {
 				resultWriter.setStatus(BridgeTCKResultWriter.FAIL);
-				resultWriter.setDetail("Incorrect Specification Version: " + version + " should be '2.0'");
+				resultWriter.setDetail("Incorrect Specification Version: " + version + " should be '5.0.0'");
 			}
 			else {
 				resultWriter.setStatus(BridgeTCKResultWriter.PASS);
