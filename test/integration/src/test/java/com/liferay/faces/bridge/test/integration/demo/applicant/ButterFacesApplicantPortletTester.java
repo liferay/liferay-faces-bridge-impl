998a918a36ba066113fbc6ef8b8d79da4e77d1f0
Kyle Stiemann <stiemannkj1@gmail.com>
2017-07-27 15:36:29 -0400

    FACES-3143 Develop butterfaces-applicant-portlet (develop tester)

--- test/integration/src/test/java/com/liferay/faces/bridge/test/integration/demo/applicant/ButterFacesApplicantPortletTester.java
+++ test/integration/src/test/java/com/liferay/faces/bridge/test/integration/demo/applicant/ButterFacesApplicantPortletTester.java
@@ -18,28 +18,61 @@ package com.liferay.faces.bridge.test.integration.demo.applicant;
 import org.junit.Assume;
 import org.junit.Before;
 
+import com.liferay.faces.test.selenium.browser.BrowserDriver;
+import com.liferay.faces.test.selenium.browser.TestUtil;
+import com.liferay.faces.test.selenium.browser.WaitingAsserter;
+
 
 /**
  * @author  Kyle Stiemann
  */
 public class ButterFacesApplicantPortletTester extends BridgeApplicantPortletTester {
 
-	@Before
-	public void skipButterFaces() {
+	@Override
+	public void runApplicantPortletTest_E_AllFieldsRequired() {
 
-		// Skip ButterFaces until certain issues are fixed and the error message tests are overridden to detect
-		// ButterFaces tooltip error messages.
-		Assume.assumeTrue(false);
+		BrowserDriver browserDriver = getBrowserDriver();
+		clearAllFields(browserDriver);
+		browserDriver.clickElementAndWaitForRerender(getSubmitButtonXpath());
+
+		// Verify that 8 "Value is required" messages appear.
+		WaitingAsserter waitingAsserter = getWaitingAsserter();
+		waitingAsserter.assertElementDisplayed(getFieldErrorXpath(getFirstNameFieldXpath()) +
+			"[contains(text(),'Value is required')][8]");
+	}
+
+	@Before
+	public void skipTestOnLiferay62() {
+
+		// BootsFaces is unsupported on Liferay 6.2 due to Bootstrap incompatibilities.
+		Assume.assumeFalse("liferay62".equals(TestUtil.getContainer()));
 	}
 
 	@Override
 	protected String getFieldErrorXpath(String fieldXpath) {
 		return fieldXpath +
-			"/../../following-sibling::div[@role='tooltip']//div[contains(@class,'butter-component-tooltip')][contains(@class,'error')]/ul/li";
+			"/../../../../../preceding-sibling::ul[contains(@id,'globalMessages')]/li[contains(@class,'portlet-msg-error')]";
 	}
 
 	@Override
 	protected String getPortletPageName() {
 		return "butterfaces-applicant";
+	}
+
+	@Override
+	protected String getSubmitFileButtonXpath() {
+		return "//form[@method='post'][@enctype='multipart/form-data']/a[contains(.,'Submit')]";
+	}
+
+	@Override
+	protected void selectDate(BrowserDriver browserDriver) {
+
+		String dateOfBirthFieldXpath = getDateOfBirthFieldXpath();
+		sendKeysTabAndWaitForRerender(browserDriver, dateOfBirthFieldXpath, "01/02/3456");
+		browserDriver.clickElement("//span[contains(@class, 'icon-calendar')]");
+
+		String dateCellXpath = "//div[contains(@class, 'datepicker-days')]//td[contains(text(), '14')]";
+		browserDriver.waitForElementEnabled(dateCellXpath);
+		browserDriver.clickElement(dateCellXpath);
 	}
 }
