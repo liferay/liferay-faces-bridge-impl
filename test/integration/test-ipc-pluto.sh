6d6bc74b3e8eaaa27b3f72e1fd069dee6eec65e7
Neil Griffin <neil.griffin.scm@gmail.com>
2017-07-28 15:04:10 -0400

    FACES-2905 ICEfaces 1.8 portlets are unsupported on Pluto 2.0

--- test/integration/test-ipc-pluto.sh
+++ test/integration/test-ipc-pluto.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mvn verify -Dintegration.port=9080 -P selenium,pluto -Dit.test=\*IPC\*
+# NOTE: It is not possible to test the icefaces-ipc-ajax-push-portlet on Pluto: https://issues.liferay.com/browse/FACES-3163
+mvn verify -Dintegration.port=9080 -P selenium,pluto -Dit.test=\*JSF_IPC\*
