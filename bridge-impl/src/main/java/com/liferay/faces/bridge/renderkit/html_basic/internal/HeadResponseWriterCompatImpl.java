219a12a27be5b16b93e8f7f2448042042b2be800
Kyle Stiemann <stiemannkj1@gmail.com>
2017-09-28 08:29:45 -0400

    FACES-3231 HeadResponseWriters render nested elements as direct children of <head>

--- bridge-impl/src/main/java/com/liferay/faces/bridge/renderkit/html_basic/internal/HeadResponseWriterCompatImpl.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/renderkit/html_basic/internal/HeadResponseWriterCompatImpl.java
@@ -16,6 +16,8 @@
 package com.liferay.faces.bridge.renderkit.html_basic.internal;
 
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Locale;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.ResponseWriter;
@@ -23,6 +25,7 @@ import javax.portlet.MimeResponse;
 import javax.portlet.RenderResponse;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
@@ -30,7 +33,9 @@ import com.liferay.faces.util.logging.LoggerFactory;
 
 /**
  * Custom {@link ResponseWriter} that has the ability to write to the &lt;head&gt;...&lt;/head&gt; section of the portal
- * page.
+ * page. Because {@link RenderResponse} only supports the creation of {@link Element}s via {@link
+ * RenderResponse#createElement(java.lang.String)}, many features of normal ResponseWriters such as CDATA, comments, and
+ * child text nodes must be disabled since those node types cannot be created.
  *
  * @author  Neil Griffin
  */
@@ -45,6 +50,48 @@ public class HeadResponseWriterCompatImpl extends HeadResponseWriterBase {
 	public HeadResponseWriterCompatImpl(ResponseWriter wrappedResponseWriter, RenderResponse renderResponse) {
 		super(wrappedResponseWriter);
 		this.renderResponse = renderResponse;
+	}
+
+	@Override
+	public Writer append(CharSequence csq) throws IOException {
+
+		if (csq != null) {
+
+			String text = csq.toString();
+
+			if ("<![CDATA[".equalsIgnoreCase(text.toUpperCase(Locale.ENGLISH))) {
+				startCDATA();
+			}
+			else if ("]]>".equalsIgnoreCase(text)) {
+				endCDATA();
+			}
+			else {
+
+				Node currentNode = getCurrentNode();
+
+				if (!currentNode.hasChildNodes()) {
+
+					String textContent = currentNode.getTextContent();
+
+					if (textContent == null) {
+						textContent = text;
+					}
+					else {
+						textContent += text;
+					}
+
+					currentNode.setTextContent(textContent);
+				}
+
+				// Avoid overwriting child nodes with text.
+				else {
+					logger.warn(
+						"Element text removed because adding both children and text to an element in the <head> section is not supported in Portlet 2.0.");
+				}
+			}
+		}
+
+		return this;
 	}
 
 	@Override
@@ -65,34 +112,26 @@ public class HeadResponseWriterCompatImpl extends HeadResponseWriterBase {
 	}
 
 	@Override
-	protected void addResourceToHeadSection(Element element, String nodeName, UIComponent componentResource)
-		throws IOException {
+	protected Node createElement(String nodeName) {
+		return renderResponse.createElement(nodeName);
+	}
+
+	@Override
+	protected void writeNodeToHeadSection(Node node, UIComponent componentResource) throws IOException {
+
+		if (!isElement(node)) {
+			throw new IllegalArgumentException("Only elements may be added to the <head> section in Portlet 2.0");
+		}
 
 		// NOTE: The Portlet 2.0 Javadocs for the addProperty method indicate that if the key already exists,
 		// then the element will be added to any existing elements under that key name. There is a risk that
 		// multiple portlet instances on the same portal page could cause multiple <script /> elements to be
 		// added to the <head>...</head> section of the rendered portal page. See:
 		// http://portals.apache.org/pluto/portlet-2.0-apidocs/javax/portlet/PortletResponse.html#addProperty(java.lang.String,%20org.w3c.dom.Element)
-		renderResponse.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, element);
-		logger.debug("Added resource to {0}'s <head>...</head> section, element=[{1}]", "portal", nodeName);
-	}
+		renderResponse.addProperty(MimeResponse.MARKUP_HEAD_ELEMENT, (Element) node);
 
-	@Override
-	protected Element createElement(String name) {
-		return renderResponse.createElement(name);
-	}
-
-	@Override
-	protected boolean isEscapeAttributeValueXML(Element currentElement) {
-
-		// The text will be escaped by the portlet container handling the element. See FACES-3220 for more details.
-		return false;
-	}
-
-	@Override
-	protected boolean isEscapeTextXML(Element currentElement) {
-
-		// The text will be escaped by the portlet container handling the element. See FACES-3220 for more details.
-		return false;
+		if (logger.isDebugEnabled()) {
+			logger.debug("Added resource to Liferay's <head>...</head> section, node=[{0}]", getNodeInfo(node));
+		}
 	}
 }
