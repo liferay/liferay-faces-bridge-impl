5d8781a2cacf4bc7df60e13f2197246b309fb4c1
Kyle Stiemann <stiemannkj1@gmail.com>
2017-10-19 14:43:54 -0400

    FACES-3235 Consolidate non-PrimeFaces issue portlets into a single issues war

---
+++ issue/jsf-issues-portlet/src/main/java/com/liferay/faces/issue/FACES_1470/AS7LeakRequestScopeBean.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.issue.FACES_1470;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@ManagedBean(name = "as7LeakRequestScopeBean")
+@RequestScoped
+public class AS7LeakRequestScopeBean {
+
+	// Private Data Members
+	private String foo = this.toString();
+
+	public AS7LeakRequestScopeBean() {
+		InstanceTrackerBean.trackAS7LeakInstance(this);
+	}
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+}
---
+++ issue/jsf-issues-portlet/src/main/java/com/liferay/faces/issue/FACES_1618/RenderKitFactoryImpl.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.issue.FACES_1618;
+
+import java.util.Iterator;
+
+import javax.faces.application.Application;
+import javax.faces.context.FacesContext;
+import javax.faces.render.RenderKit;
+import javax.faces.render.RenderKitFactory;
+import javax.portlet.PortletConfig;
+
+import com.liferay.faces.util.application.ApplicationUtil;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class RenderKitFactoryImpl extends RenderKitFactory {
+
+	// Private Data Members
+	private RenderKitFactory wrappedRenderKitFactory;
+
+	public RenderKitFactoryImpl(RenderKitFactory renderKitFactory) {
+		this.wrappedRenderKitFactory = renderKitFactory;
+	}
+
+	@Override
+	public void addRenderKit(String renderKitId, RenderKit renderKit) {
+		wrappedRenderKitFactory.addRenderKit(renderKitId, renderKit);
+	}
+
+	@Override
+	public RenderKit getRenderKit(FacesContext facesContext, String renderKitId) {
+
+		RenderKit renderKit = wrappedRenderKitFactory.getRenderKit(facesContext, renderKitId);
+
+		// FACES-2615 Only Add the RenderKit to the delegation chain when the application is not starting up or
+		// shutting down.
+		if (!ApplicationUtil.isStartupOrShutdown(facesContext)) {
+
+			Application application = facesContext.getApplication();
+			PortletConfig portletConfig = application.evaluateExpressionGet(facesContext, "#{portletConfig}",
+					PortletConfig.class);
+			String portletName = portletConfig.getPortletName();
+
+			if ((portletName.startsWith("FACES") && portletName.endsWith("1618")) &&
+					("HTML_BASIC".equals(renderKitId) || "PRIMEFACES_MOBILE".equals(renderKitId))) {
+				renderKit = new RenderKitImpl(renderKit);
+			}
+		}
+
+		return renderKit;
+	}
+
+	@Override
+	public Iterator<String> getRenderKitIds() {
+		return wrappedRenderKitFactory.getRenderKitIds();
+	}
+}
