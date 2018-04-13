3cbff5977aa3c565943c8f8c59c409636dae1ae2
Kyle Stiemann <stiemannkj1@gmail.com>
2018-04-12 20:57:49 -0400

    FACES-3266 Add FACES-1513-2185-portlet to primefaces-issues and create Selenium tester to test for regressions

---
+++ issue/primefaces-issues-portlet/src/main/java/com/liferay/faces/issue/primefaces/FACES_1513_2185/User.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.issue.primefaces.FACES_1513_2185;
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class User {
+
+	// Private Final Data Members
+	private final String firstName;
+	private final String lastName;
+
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+}
