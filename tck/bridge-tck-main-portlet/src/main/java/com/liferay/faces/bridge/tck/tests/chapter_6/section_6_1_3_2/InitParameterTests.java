15c28edbf65aa989a9eb9429657459e214198fd3
Neil Griffin <neil.griffin.scm@gmail.com>
2018-06-30 14:08:46 -0400

    FACES-3078 Refactor Bridge TCK (Rename m -> map)

--- tck/bridge-tck-main-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter_6/section_6_1_3_2/InitParameterTests.java
+++ tck/bridge-tck-main-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter_6/section_6_1_3_2/InitParameterTests.java
@@ -218,13 +218,13 @@ public class InitParameterTests {
 		}
 	}
 
-	private boolean containsIdenticalInitParamEntries(Map<String, String> m, Enumeration<String> eNames,
+	private boolean containsIdenticalInitParamEntries(Map<String, String> map, Enumeration<String> eNames,
 		PortletConfig portletConfig) {
 
-		// For each entry in m ensure there is an identical one in the context
+		// For each entry in map ensure there is an identical one in the context
 		PortletContext portletContext = portletConfig.getPortletContext();
 
-		for (Iterator<Map.Entry<String, String>> entries = m.entrySet().iterator(); entries.hasNext();) {
+		for (Iterator<Map.Entry<String, String>> entries = map.entrySet().iterator(); entries.hasNext();) {
 			Map.Entry<String, String> e = entries.next();
 			String value = portletConfig.getInitParameter(e.getKey());
 
@@ -251,7 +251,7 @@ public class InitParameterTests {
 				value = portletContext.getInitParameter(key);
 			}
 
-			Object mapObj = m.get(key);
+			Object mapObj = map.get(key);
 
 			if ((mapObj == null) && (value == null))
 				continue; // technically shouldn't have this but some container do
