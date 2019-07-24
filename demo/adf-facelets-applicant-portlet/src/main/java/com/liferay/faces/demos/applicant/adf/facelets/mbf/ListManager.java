b66c50a67f561f914c897fbec4eb03563c163864
Neil Griffin <neil.griffin.scm@gmail.com>
2019-07-23 20:35:56 -0400

    FACES-3460 Support ADF Faces (make consistent with other applicant demo portlets)

--- demo/adf-facelets-applicant-portlet/src/main/java/com/liferay/faces/demos/applicant/adf/facelets/mbf/ListManager.java
+++ demo/adf-facelets-applicant-portlet/src/main/java/com/liferay/faces/demos/applicant/adf/facelets/mbf/ListManager.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.liferay.faces.demos.applicant.adf.facelets.bean;
+package com.liferay.faces.demos.applicant.adf.facelets.mbf;
 
 import java.util.ArrayList;
 import java.util.Collections;
