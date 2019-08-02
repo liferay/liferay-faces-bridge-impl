cfc00061e86f0cfcd6a64dfb193f506af6cacb7e
Kyle Stiemann <stiemannkj1@gmail.com>
2019-08-02 13:45:13 -0400

    FACES-3024 Enable deployment of Liferay Faces dependencies as OSGi bundles (add config necessary to deploy WARs as thin WABs)

--- test/integration/src/test/java/com/liferay/faces/bridge/test/integration/demo/JSFFlowsPortletTester.java
+++ test/integration/src/test/java/com/liferay/faces/bridge/test/integration/demo/JSFFlowsPortletTester.java
@@ -55,11 +55,9 @@ public class JSFFlowsPortletTester extends BrowserDriverManagingTesterBase {
 
 		if (TestUtil.getContainer().contains("liferay")) {
 			assertLibraryElementDisplayed(waitingAsserter, "Liferay Faces Bridge Ext", browserDriver);
-			assertLibraryElementDisplayed(waitingAsserter, "JCDI", browserDriver);
 		}
-		else {
-			assertLibraryElementDisplayed(waitingAsserter, "Weld", browserDriver);
-		}
+
+		assertLibraryElementDisplayed(waitingAsserter, "CDI", browserDriver);
 
 		// Test that exiting the bookings flow scope causes beans to go out of scope.
 		String enterBookingFlowButtonXpath = "//input[@value='Enter Booking Flow']";
