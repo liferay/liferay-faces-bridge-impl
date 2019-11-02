55a04748a9f6834ce5cb20883be8cc3b50b50f04
Neil Griffin <neil.griffin.scm@gmail.com>
2019-11-02 15:42:59 -0400

    Disable headless browser testing for test scripts

--- tck/bridge-tck-main-portlet/test-pluto-tomcat.sh
+++ tck/bridge-tck-main-portlet/test-pluto-tomcat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn -Dintegration.port=9080 verify -P selenium,pluto,chrome,tomcat "$@"
+mvn -Dintegration.port=9080 verify -P selenium,pluto,chrome,tomcat "$@" -Dintegration.browser.headless=false
--- test/integration/test-flows-liferay.sh
+++ test/integration/test-flows-liferay.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn verify -P selenium,liferay -Dit.test=\*Flows\*
+mvn verify -P selenium,liferay -Dit.test=\*Flows\* -Dintegration.browser.headless=false
