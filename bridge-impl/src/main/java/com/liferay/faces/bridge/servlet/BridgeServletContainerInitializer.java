2121c070d4415ddb208276d80e95493a877dc852
Neil Griffin <neil.griffin.scm@gmail.com>
2022-01-26 13:24:24 -0500

    FACES-3530 Upgrade to Mojarra 2.3.9 (accommodate Mojarra's detection of CDI during startup)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/servlet/BridgeServletContainerInitializer.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/servlet/BridgeServletContainerInitializer.java
@@ -17,18 +17,29 @@ package com.liferay.faces.bridge.servlet;
 
 import java.util.Set;
 
+import javax.faces.annotation.FacesConfig;
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.HandlesTypes;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 
 /**
  * @author  Kyle Stiemann
+ * @author  Neil Griffin
  */
+@HandlesTypes(FacesConfig.class)
 public final class BridgeServletContainerInitializer implements ServletContainerInitializer {
 
 	@Override
 	public void onStartup(Set<Class<?>> annotatedClasses, ServletContext servletContext) throws ServletException {
+
+		if ((annotatedClasses != null) && !annotatedClasses.isEmpty()) {
+			servletContext.setAttribute(FacesConfig.class.getName(), Boolean.TRUE);
+		}
+
 		servletContext.addListener(BridgeSessionListener.class);
 	}
 }
