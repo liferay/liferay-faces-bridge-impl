23f95a96d14b05a413ab8b4a6fcc7238f722707c
Neil Griffin <neil.griffin.scm@gmail.com>
2022-01-28 17:10:02 -0500

    FACES-3625 Some bridge issues portlets are blank

--- tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/BridgeRequestScopedBeanTestPortlet.java
+++ tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/BridgeRequestScopedBeanTestPortlet.java
@@ -28,7 +28,7 @@ import com.liferay.faces.bridge.tck.common.portlet.GenericFacesTestSuitePortlet;
  * @author  Neil Griffin
  */
 @PortletConfiguration(
-	portletName = "chapter7_2CDITests-bridgeRequestScopedBeanTest-portlet",
+	portletName = "chapter7_2CDITestsbridgeRequestScopedBeanTestportlet",
 	initParams = {
 			@InitParameter(
 				name = "javax.portlet.faces.defaultViewId.view", value = "/WEB-INF/views/multiRequestTest.xhtml"
@@ -39,7 +39,7 @@ import com.liferay.faces.bridge.tck.common.portlet.GenericFacesTestSuitePortlet;
 		}
 )
 @LiferayPortletConfiguration(
-	portletName = "chapter7_2CDITests-bridgeRequestScopedBeanTest-portlet",
+	portletName = "chapter7_2CDITestsbridgeRequestScopedBeanTestportlet",
 	properties = {
 			"com.liferay.portlet.header-request-attribute-prefix=false",
 			"com.liferay.portlet.requires-namespaced-parameters=false"
