119453c0b3941964d1d14322c7593afc1b3228f4
Neil Griffin <neil.griffin.scm@gmail.com>
2022-02-01 15:56:13 -0500

    FACES-3107 Require the FacesBridge to provide @Alternative CDI producers for Portlet 3.0 @Named beans that can be wrapped by factories (actionResponseAlternativeTest)

--- tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/Tests.java
+++ tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/Tests.java
@@ -177,7 +177,7 @@ public class Tests {
 
 			return "multiRequestTestResultRenderCheck";
 		}
-		else if (portletPhase == Bridge.PortletPhase.HEADER_PHASE) {
+		else if (portletPhase == Bridge.PortletPhase.RENDER_PHASE) {
 
 			if ("ActionResponseTCKImpl".equals(bridgeRequestScopedBean.getFoo())) {
 
