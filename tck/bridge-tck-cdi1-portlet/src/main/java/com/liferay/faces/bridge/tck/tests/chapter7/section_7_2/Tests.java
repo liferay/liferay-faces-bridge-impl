710e3edc20fc9f6aa0782819434a2c3c593c7a8b
Neil Griffin <neil.griffin.scm@gmail.com>
2022-02-01 16:07:25 -0500

    FACES-3107 Require the FacesBridge to provide @Alternative CDI producers for Portlet 3.0 @Named beans that can be wrapped by factories (clientDataRequestAlternativeTest)

--- tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/Tests.java
+++ tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/Tests.java
@@ -21,6 +21,7 @@ import javax.inject.Named;
 import javax.portlet.ActionParameters;
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.ClientDataRequest;
 import javax.portlet.PortletConfig;
 import javax.portlet.PortletContext;
 import javax.portlet.PortletMode;
@@ -70,6 +71,9 @@ public class Tests {
 
 	@Inject
 	private CDIRequestScopedBean cdiRequestScopedBean;
+
+	@Inject
+	private ClientDataRequest clientDataRequest;
 
 	@Inject
 	private PortletConfig portletConfig;
@@ -273,6 +277,40 @@ public class Tests {
 			}
 			else {
 				testBean.setTestResult(false, "CDI @RequestScoped is behaving like @BridgeRequestScoped");
+
+				return Constants.TEST_FAILED;
+			}
+		}
+
+		testBean.setTestResult(false, "Unexpected portletPhase=" + portletPhase);
+
+		return Constants.TEST_FAILED;
+	}
+
+	@BridgeTest(test = "clientDataRequestAlternativeTest")
+	public String clientDataRequestAlternativeTest(TestBean testBean) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		Bridge.PortletPhase portletPhase = BridgeUtil.getPortletRequestPhase(facesContext);
+
+		if (portletPhase == Bridge.PortletPhase.ACTION_PHASE) {
+
+			bridgeRequestScopedBean.setFoo(clientDataRequest.getClass().getSimpleName());
+
+			return "multiRequestTestResultRenderCheck";
+		}
+		else if (portletPhase == Bridge.PortletPhase.HEADER_PHASE) {
+
+			if ("ActionRequestTCKImpl".equals(bridgeRequestScopedBean.getFoo())) {
+
+				testBean.setTestResult(true,
+					"The bridge's alternative producer for ClientDataRequest was properly invoked");
+
+				return Constants.TEST_SUCCESS;
+			}
+			else {
+				testBean.setTestResult(false,
+					"The bridge's alternative producer for ClientDataRequest was not invoked");
 
 				return Constants.TEST_FAILED;
 			}
