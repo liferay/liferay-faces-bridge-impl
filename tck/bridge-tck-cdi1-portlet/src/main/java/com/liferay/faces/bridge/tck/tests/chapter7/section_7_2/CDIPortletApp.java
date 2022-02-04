3375c7369255a4a02f416f2f4ff79e6d0fff4ae1
Neil Griffin <neil.griffin.scm@gmail.com>
2022-02-04 14:26:21 -0500

    FACES-3107 Require the FacesBridge to provide @Alternative CDI producers for Portlet 3.0 @Named beans that can be wrapped by factories (eventRequestAlternativeTest)

---
+++ tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/CDIPortletApp.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2021 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.tck.tests.chapter7.section_7_2;
+
+import javax.portlet.annotations.EventDefinition;
+import javax.portlet.annotations.PortletApplication;
+import javax.portlet.annotations.PortletQName;
+
+
+/**
+ * @author  Neil Griffin
+ */
+
+@PortletApplication(
+	events = {
+			@EventDefinition(
+				qname =
+					@PortletQName(
+						localPart = "faces.liferay.com.tck.testEvent",
+						namespaceURI = "http://liferay.com/faces/event_ns"
+					), payloadType = String.class
+			),
+		}
+)
+public class CDIPortletApp {
+}
---
+++ tck/bridge-tck-cdi1-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter7/section_7_2/Ch7TestEventHandler.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2021 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.tck.tests.chapter7.section_7_2;
+
+import javax.el.ELContext;
+import javax.el.ELResolver;
+import javax.faces.context.FacesContext;
+import javax.portlet.Event;
+import javax.portlet.faces.BridgeEventHandler;
+import javax.portlet.faces.event.EventNavigationResult;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class Ch7TestEventHandler implements BridgeEventHandler {
+
+	@Override
+	public EventNavigationResult handleEvent(FacesContext facesContext, Event event) {
+
+		String testName = (String) event.getValue();
+
+		if ("eventRequestAlternativeTest".equals(testName)) {
+
+			ELContext elContext = facesContext.getELContext();
+			ELResolver elResolver = elContext.getELResolver();
+
+			TestsCDI1 tests = (TestsCDI1) elResolver.getValue(elContext, null, "chapter7_2CDITests");
+			tests.eventRequestAlternativeTest(null);
+		}
+
+		return null;
+	}
+}
