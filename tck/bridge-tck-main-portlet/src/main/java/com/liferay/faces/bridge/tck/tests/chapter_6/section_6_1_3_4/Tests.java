abf184c0ff4282660d0ca3868d467213210fef02
Neil Griffin <neil.griffin.scm@gmail.com>
2022-02-19 14:45:02 -0500

    FACES-3519 Support ExternalContext.getSessionMaxInactiveInterval()

---
+++ tck/bridge-tck-main-portlet/src/main/java/com/liferay/faces/bridge/tck/tests/chapter_6/section_6_1_3_4/Tests.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-2021 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.bridge.tck.tests.chapter_6.section_6_1_3_4;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.portlet.PortletSession;
+
+import com.liferay.faces.bridge.tck.annotation.BridgeTest;
+import com.liferay.faces.bridge.tck.beans.TestBean;
+import com.liferay.faces.bridge.tck.common.Constants;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class Tests {
+
+	// Test 6.157
+	@BridgeTest(test = "getSessionMaxInactiveIntervalTest")
+	public String getSessionMaxInactiveIntervalTest(TestBean testBean) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		PortletSession portletSession = (PortletSession) externalContext.getSession(true);
+
+		if (portletSession.getMaxInactiveInterval() == externalContext.getSessionMaxInactiveInterval()) {
+			testBean.setTestResult(true, "ExternalContext.getSessionMaxInactiveInterval() returned the correct value");
+
+			return Constants.TEST_SUCCESS;
+		}
+
+		testBean.setTestResult(false, "ExternalContext.getSessionMaxInactiveInterval() returned an incorrect value");
+
+		return Constants.TEST_FAILED;
+	}
+}
