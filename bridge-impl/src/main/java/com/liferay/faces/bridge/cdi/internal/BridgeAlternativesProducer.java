0b6a0c736dab36b7b83ade1cbfb53f3a0999e6c2
Neil Griffin <neil.griffin.scm@gmail.com>
2022-03-16 20:32:06 -0400

    FACES-3107 Require the FacesBridge to provide @Alternative CDI producers for Portlet 3.0 @Named beans that can be wrapped by factories (Pluto compatibility)

--- bridge-impl/src/main/java/com/liferay/faces/bridge/cdi/internal/BridgeAlternativesProducer.java
+++ bridge-impl/src/main/java/com/liferay/faces/bridge/cdi/internal/BridgeAlternativesProducer.java
@@ -15,18 +15,26 @@
  */
 package com.liferay.faces.bridge.cdi.internal;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.Alternative;
+import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.Typed;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.util.AnnotationLiteral;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.interceptor.Interceptor;
 import javax.portlet.ActionRequest;
@@ -51,6 +59,8 @@ import javax.portlet.WindowState;
 import javax.servlet.http.Cookie;
 
 import com.liferay.faces.bridge.util.internal.RequestMapUtil;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
 
 
 /**
@@ -61,12 +71,18 @@ import com.liferay.faces.bridge.util.internal.RequestMapUtil;
 @Priority(Interceptor.Priority.APPLICATION + 10)
 public class BridgeAlternativesProducer {
 
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(BridgeAlternativesProducer.class);
+
+	@Inject
+	private BeanManager beanManager;
+
 	@Named("actionRequest")
 	@Dependent
 	@Produces
 	@Typed(ActionRequest.class)
 	public ActionRequest getActionRequest() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest(ActionRequest.class);
 
 		if ((portletRequest != null) && (portletRequest instanceof ActionRequest)) {
 
@@ -83,7 +99,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(ActionResponse.class)
 	public ActionResponse getActionResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(ActionResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof ActionResponse)) {
 			return (ActionResponse) portletResponse;
@@ -97,7 +113,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(ClientDataRequest.class)
 	public ClientDataRequest getClientDataRequest() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest(ClientDataRequest.class);
 
 		if ((portletRequest != null) && (portletRequest instanceof ClientDataRequest)) {
 
@@ -113,7 +129,7 @@ public class BridgeAlternativesProducer {
 	@Named("cookies")
 	@Produces
 	public List<Cookie> getCookies() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest();
 
 		if (portletRequest == null) {
 			return null;
@@ -133,7 +149,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(EventRequest.class)
 	public EventRequest getEventRequest() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest(EventRequest.class);
 
 		if ((portletRequest != null) && (portletRequest instanceof EventRequest)) {
 			return (EventRequest) portletRequest;
@@ -147,7 +163,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(EventResponse.class)
 	public EventResponse getEventResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(EventResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof EventResponse)) {
 			return (EventResponse) portletResponse;
@@ -160,7 +176,7 @@ public class BridgeAlternativesProducer {
 	@Named("locales")
 	@Produces
 	public List<Locale> getLocales() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest();
 
 		if (portletRequest == null) {
 			return null;
@@ -174,7 +190,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(MimeResponse.class)
 	public MimeResponse getMimeResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(MimeResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof MimeResponse)) {
 			return (MimeResponse) portletResponse;
@@ -189,6 +205,10 @@ public class BridgeAlternativesProducer {
 	public PortletConfig getPortletConfig() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 
+		if (facesContext == null) {
+			return _getNonAlternativeDefaultBean(PortletConfig.class);
+		}
+
 		return RequestMapUtil.getPortletConfig(facesContext);
 	}
 
@@ -198,6 +218,10 @@ public class BridgeAlternativesProducer {
 	public PortletContext getPortletContext() {
 		PortletConfig portletConfig = getPortletConfig();
 
+		if (portletConfig == null) {
+			return _getNonAlternativeDefaultBean(PortletContext.class);
+		}
+
 		return portletConfig.getPortletContext();
 	}
 
@@ -205,7 +229,11 @@ public class BridgeAlternativesProducer {
 	@Named(value = "portletMode")
 	@Produces
 	public PortletMode getPortletMode() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest();
+
+		if (portletRequest == null) {
+			return null;
+		}
 
 		return portletRequest.getPortletMode();
 	}
@@ -214,7 +242,11 @@ public class BridgeAlternativesProducer {
 	@Named(value = "portletPreferences")
 	@Produces
 	public PortletPreferences getPortletPreferences() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest();
+
+		if (portletRequest == null) {
+			return null;
+		}
 
 		return portletRequest.getPreferences();
 	}
@@ -233,10 +265,7 @@ public class BridgeAlternativesProducer {
 	@Dependent
 	@Produces
 	public PortletResponse getPortletResponse() {
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		ExternalContext externalContext = facesContext.getExternalContext();
-
-		return (PortletResponse) externalContext.getResponse();
+		return _getPortletResponse();
 	}
 
 	@Named("portletSession")
@@ -244,6 +273,11 @@ public class BridgeAlternativesProducer {
 	@Produces
 	public PortletSession getPortletSession() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		if (facesContext == null) {
+			return _getNonAlternativeDefaultBean(PortletSession.class);
+		}
+
 		ExternalContext externalContext = facesContext.getExternalContext();
 
 		return (PortletSession) externalContext.getSession(true);
@@ -254,7 +288,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(RenderRequest.class)
 	public RenderRequest getRenderRequest() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest(RenderRequest.class);
 
 		if ((portletRequest != null) && (portletRequest instanceof RenderRequest)) {
 			return (RenderRequest) portletRequest;
@@ -268,7 +302,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(RenderResponse.class)
 	public RenderResponse getRenderResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(RenderResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof RenderResponse)) {
 			return (RenderResponse) portletResponse;
@@ -282,7 +316,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(ResourceRequest.class)
 	public ResourceRequest getResourceRequest() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest(ResourceRequest.class);
 
 		if ((portletRequest != null) && (portletRequest instanceof ResourceRequest)) {
 			return (ResourceRequest) portletRequest;
@@ -296,7 +330,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(ResourceResponse.class)
 	public ResourceResponse getResourceResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(ResourceResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof ResourceResponse)) {
 			return (ResourceResponse) portletResponse;
@@ -310,7 +344,7 @@ public class BridgeAlternativesProducer {
 	@Produces
 	@Typed(StateAwareResponse.class)
 	public StateAwareResponse getStateAwareResponse() {
-		PortletResponse portletResponse = getPortletResponse();
+		PortletResponse portletResponse = _getPortletResponse(StateAwareResponse.class);
 
 		if ((portletResponse != null) && (portletResponse instanceof StateAwareResponse)) {
 			return (StateAwareResponse) portletResponse;
@@ -323,8 +357,84 @@ public class BridgeAlternativesProducer {
 	@Named(value = "windowState")
 	@Produces
 	public WindowState getWindowState() {
-		PortletRequest portletRequest = getPortletRequest();
+		PortletRequest portletRequest = _getPortletRequest();
+
+		if (portletRequest == null) {
+			return null;
+		}
 
 		return portletRequest.getWindowState();
+	}
+
+	@Named("portletRequest")
+	@Dependent
+	@Produces
+	public PortletRequest ggetPortletRequest() {
+		return _getPortletRequest();
+	}
+
+	/**
+	 * When the Liferay Faces Bridge Impl jar module is deployed in the global classpath (such as with Apache
+	 * Pluto+Tomcat), the alternative producers in this class are invoked in favor of the non-alternative (default)
+	 * producers that are provided by the portal. When a JSF portlet is causing these producers to be invoked, all is
+	 * well since FacesContext.getCurrentInstance() returns a non-null value. But when a plain Portlet 3.0 bean portlet
+	 * causes these producers to be invoked, FacesContext.getCurrentInstance() returns a null value. In this case, it is
+	 * necessary to programatically acquire the non-alternative (default) producer.
+	 */
+	private <T> T _getNonAlternativeDefaultBean(Class<T> clazz) {
+		Set<Bean<?>> beans = beanManager.getBeans(clazz, DefaultAnnotation.INSTANCE);
+
+		for (Bean<?> bean : beans) {
+			Class<?> beanClass = bean.getBeanClass();
+			String beanClassName = beanClass.getName();
+
+			if (!beanClassName.contains(getClass().getPackage().getName())) {
+				CreationalContext<?> creationalContext = beanManager.createCreationalContext(bean);
+
+				Object beanInstance = beanManager.getReference(bean, clazz, creationalContext);
+
+				logger.debug("beanClassName=[{}] beanInstance=[{}]", beanClassName, beanInstance);
+
+				return (T) beanInstance;
+			}
+		}
+
+		return null;
+	}
+
+	private PortletRequest _getPortletRequest() {
+		return _getPortletRequest(PortletRequest.class);
+	}
+
+	private <T extends PortletRequest> T _getPortletRequest(Class<T> clazz) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		if (facesContext == null) {
+			return _getNonAlternativeDefaultBean(clazz);
+		}
+
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		return (T) externalContext.getRequest();
+	}
+
+	private PortletResponse _getPortletResponse() {
+		return _getPortletResponse(PortletResponse.class);
+	}
+
+	private <T extends PortletResponse> T _getPortletResponse(Class<T> clazz) {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		if (facesContext == null) {
+			return _getNonAlternativeDefaultBean(clazz);
+		}
+
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		return (T) externalContext.getResponse();
+	}
+
+	private static class DefaultAnnotation extends AnnotationLiteral<Default> implements Default {
+		public static final DefaultAnnotation INSTANCE = new DefaultAnnotation();
 	}
 }
