20f6312c66b2db6aad6b96eeeb92d7d3e9a3d859
Neil Griffin <neil.griffin.scm@gmail.com>
2022-04-29 15:25:02 -0400

    FACES-3024 Enable deployment of Liferay Faces dependencies as OSGi bundles (maven profile for "thin" WARs -- Pluto 3.x specific)

---
+++ issue/jsf-issues-portlet/src/main/java/com/liferay/faces/issue/JSFSPEC_1433Portlet.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-2022 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.issue;
+
+import javax.portlet.annotations.InitParameter;
+import javax.portlet.annotations.PortletConfiguration;
+import javax.portlet.annotations.SecurityRoleRef;
+import javax.portlet.faces.GenericFacesPortlet;
+
+
+/**
+ * @author  Neil Griffin
+ */
+@PortletConfiguration(
+	portletName = "JSFSPEC-1433", initParams = {
+			@InitParameter(name = "javax.portlet.faces.defaultViewId.view", value = "/WEB-INF/views/JSFSPEC-1433.xhtml")
+		}, roleRefs = {
+			@SecurityRoleRef(roleName = "administrator"), @SecurityRoleRef(roleName = "guest"),
+			@SecurityRoleRef(roleName = "power-user"), @SecurityRoleRef(roleName = "user")
+		}
+)
+public class JSFSPEC_1433Portlet extends GenericFacesPortlet {
+}
