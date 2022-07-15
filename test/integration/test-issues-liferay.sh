95e36a0d2d03e065affaaa54c3fc23e416f309dc
Neil Griffin <neil.griffin.scm@gmail.com>
2022-07-14 20:38:48 -0400

    Additional test configurations

--- test/integration/test-issues-liferay.sh
+++ test/integration/test-issues-liferay.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mvn verify -P selenium,liferay -Dit.test=\*issue.\*Test\*
+#mvn verify -P selenium,liferay -Dit.test=\*1470\*Test\*
+mvn verify -P selenium,liferay  -Dit.test='*issue.*Test*,*issue.*primefaces.*Test*'
