b32549e8eca7c6d362aa9d46390afcd950e589ba
Neil Griffin <neil.griffin.scm@gmail.com>
2022-07-19 17:58:32 -0400

    Update with comments that reflect this branch (Portlet 3.0 + JSF/Mojarra 2.3)

--- test/integration/test-applicant-pluto.sh
+++ test/integration/test-applicant-pluto.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
->&2 echo "NOTE: There are test failures in the alloy-applicant-portlet due to https://issues.liferay.com/browse/FACES-2866"
+>&2 echo "NOTE: There are test failures in the bootsfaces-applicant-portlet due to https://issues.liferay.com/browse/FACES-3683"
 >&2 echo "NOTE: There are test failures in the icefaces-applicant-portlet due to https://issues.liferay.com/browse/FACES-2867"
->&2 echo "NOTE: There are test failures in the jsf-cdi-applicant-portlet due to https://issues.liferay.com/browse/FACES-2865"
->&2 echo "NOTE: There are test failures in the jsf-jsp-applicant-portlet due to https://issues.liferay.com/browse/FACES-3054"
-mvn verify -Dintegration.port=9080 -P selenium,pluto -Dit.test=\*Applicant\*
+>&2 echo "NOTE: There are test failures in the richfaces-applicant-portlet due to https://issues.liferay.com/browse/FACES-3571"
+mvn verify -Dintegration.port=9080 -P selenium,pluto -Dit.test="!ADF*ApplicantPortletTester,!*Ice*Applicant*,!*Rich*Applicant*,*Applicant*"
