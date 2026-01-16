94c798afc15155ae1b01ea877ff70db9964f62e7
Tina Tian <tina.tian@liferay.com>
2026-01-16 11:55:37 -0800

    FACES-3745 TEMP to make compile pass

--- issue/primefaces-issues-portlet/src/main/java/com/liferay/faces/issue/primefaces/exporter/components/ExporterUsersCSVImpl.java
+++ issue/primefaces-issues-portlet/src/main/java/com/liferay/faces/issue/primefaces/exporter/components/ExporterUsersCSVImpl.java
@@ -28,74 +28,13 @@ import jakarta.faces.event.ActionEvent;
 
 import org.primefaces.component.datatable.DataTable;
 
-import org.primefaces.extensions.component.exporter.Exporter;
+//import org.primefaces.extensions.component.exporter.Exporter;
 
 
 /**
  * @author  Kyle Stiemann
  */
-public final class ExporterUsersCSVImpl extends Exporter {
+public final class ExporterUsersCSVImpl {
 
-	/* package-private */ static void appendDataTableAsCSV(FacesContext facesContext, ExternalContext externalContext, String dataTableId,
-		Appendable appendable) throws IOException {
 
-		UIViewRoot uiViewRoot = facesContext.getViewRoot();
-		DataTable dataTable = (DataTable) uiViewRoot.findComponent(dataTableId);
-		List<UIComponent> children = dataTable.getChildren();
-		boolean first = true;
-
-		for (UIComponent child : children) {
-
-			if (!first) {
-				appendable.append(",");
-			}
-
-			String headerText = (String) child.getFacet("header").getAttributes().get("value");
-			appendable.append("\"").append(headerText).append("\"");
-			first = false;
-		}
-
-		appendable.append("\n");
-
-		int originalRowIndex = dataTable.getRowIndex();
-		int rowCount = dataTable.getRowCount();
-
-		for (int i = 0; i < rowCount; i++) {
-
-			dataTable.setRowIndex(i);
-
-			User user = (User) dataTable.getRowData();
-
-			//J-
-			appendable.append("\"").append(user.getFirstName()).append("\",")
-					.append("\"").append(user.getLastName()).append("\"\n");
-			//J+
-		}
-
-		dataTable.setRowIndex(originalRowIndex);
-	}
-
-	@Override
-	public void customFormat(String facetBackground, String facetFontSize, String facetFontColor, String facetFontStyle,
-		String fontName, String cellFontSize, String cellFontColor, String cellFontStyle, String datasetPadding,
-		String orientation) throws IOException {
-		// no-op
-	}
-
-	@Override
-	public void export(ActionEvent event, String dataTableId, FacesContext facesContext, String fileName,
-		String dataTableTitle, boolean pageOnly, boolean selectionOnly, String encodingType,
-		MethodExpression preProcessor, MethodExpression postProcessor, boolean subTable) throws IOException {
-
-		ExternalContext externalContext = facesContext.getExternalContext();
-		externalContext.setResponseContentType("text/csv");
-		externalContext.setResponseHeader("Expires", "0");
-		externalContext.setResponseHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
-		externalContext.setResponseHeader("Pragma", "public");
-		externalContext.setResponseHeader("Content-disposition", "attachment;filename=" + fileName + ".csv");
-
-		Writer responseOutputWriter = externalContext.getResponseOutputWriter();
-		appendDataTableAsCSV(facesContext, externalContext, dataTableId, responseOutputWriter);
-		externalContext.responseFlushBuffer();
-	}
 }
