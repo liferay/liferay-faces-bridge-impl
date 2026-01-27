71f9522286d1ff25e6d8806ce64b35f4aebef844
Dante Wang <dante.wang@liferay.com>
2026-01-27 17:06:20 +0800

    FACES-3745 Fix file upload temp dir making in *-applicant-portlet

--- demo/alloy-applicant-portlet/src/main/java/com/liferay/faces/demos/applicant/alloy/facelets/mbf/ApplicantBacking.java
+++ demo/alloy-applicant-portlet/src/main/java/com/liferay/faces/demos/applicant/alloy/facelets/mbf/ApplicantBacking.java
@@ -100,7 +100,7 @@ public class ApplicantBacking {
 		File attachmentDir = attachmentManager.getAttachmentDir(facesContext);
 
 		if (!attachmentDir.exists()) {
-			attachmentDir.mkdir();
+			attachmentDir.mkdirs();
 		}
 
 		File copiedFile = new File(attachmentDir, uploadedFile.getName());
